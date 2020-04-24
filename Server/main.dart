c98fe1283d24c650c565512cc69496cedc776ddf
kketg <39269628+kketg@users.noreply.github.com>
2020-04-23 23:15:46 -0400

    Added delete requests, new get requests, error handling, etc

--- Server/main.dart
+++ Server/main.dart
@@ -9,9 +9,9 @@ import 'package:path/path.dart';
 Map<String, dynamic> requestTypes = {
   "GET": true,
   "POST": true,
-  "PUT": true,
+  "PUT": false,
   "HEAD": false,
-  "DELETE": false,
+  "DELETE": true,
   "PATCH": false,
   "OPTIONS": false
 };
@@ -73,7 +73,7 @@ void handleRequest(HttpRequest r) {
         break;
       case ("DELETE"):
         {
-          handlePost(r);
+          handleDelete(r);
         }
         break;
       case ("PATCH"):
@@ -93,7 +93,6 @@ void handleRequest(HttpRequest r) {
 }
 
 void handleGet(HttpRequest r) {
-  String end = "";
   String path = r.uri.path;
 
   if (path == "/query") {
@@ -103,13 +102,29 @@ void handleGet(HttpRequest r) {
     print("Queried $query for object $id");
     if (dbs.any((Collection value) => value.name == query)) {
       Collection c = dbs.singleWhere((col) => col.name == query);
-      DataObject data = c.dataList.singleWhere((d) => d.id == id);
-      end = data.toString();
+      DataObject data =
+          c.dataList.singleWhere((d) => d.id == id, orElse: () => null);
+      if (data != null) {
+        String end = data.toString();
+        var response = r.response;
+        response.write(end);
+        response.close();
+        print(end);
+      } else {
+        String end = "Object $id could not be found in $query";
+        var response = r.response;
+        response.write(end);
+        response.close();
+        print(end);
+      }
+    } else {
+      String end = "Could not find collection $query";
       print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
     }
-  }
-
-  if (path == "/query/attribute") {
+  } else if (path == "/query/attribute") {
     print("Attribute query from ${r.connectionInfo.remoteAddress}");
     String query = r.uri.queryParameters["q"];
     int id = int.parse(r.uri.queryParameters["id"]);
@@ -118,13 +133,30 @@ void handleGet(HttpRequest r) {
     if (dbs.any((Collection value) => value.name == query)) {
       Collection c = dbs.singleWhere((col) => col.name == query);
       DataObject data = c.dataList.singleWhere((d) => d.id == id);
-      AttributeContainer attribute =
-          new AttributeContainer(att, data.data[att]);
-      end = attribute.toJSON();
+      if (data.data.containsKey(att)) {
+        AttributeContainer attribute =
+            new AttributeContainer(att, data.data[att]);
+        String end = attribute.toJSON();
+        var response = r.response;
+        response.write(end);
+        response.close();
+        print(end);
+      } else {
+        String end =
+            "Attribute $att could not be found in object $id in $query";
+        var response = r.response;
+        response.write(end);
+        response.close();
+        print(end);
+      }
+    } else {
+      String end = "Could not find collection $query";
       print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
     }
-  }
-  if (path == "/query/allAttributes") {
+  } else if (path == "/query/allAttributes") {
     print("All attributes query from ${r.connectionInfo.remoteAddress}");
     String query = r.uri.queryParameters["q"];
     String att = r.uri.queryParameters["a"];
@@ -132,25 +164,63 @@ void handleGet(HttpRequest r) {
     if (dbs.any((Collection value) => value.name == query)) {
       Collection c = dbs.singleWhere((col) => col.name == query);
       List<Map<String, dynamic>> atts = new List<Map<String, dynamic>>();
-      print("Got here");
-      c.dataList.forEach((DataObject d){
-        if(d.data.containsKey(att)){
+      c.dataList.forEach((DataObject d) {
+        if (d.data.containsKey(att)) {
           AttributeContainer atc = new AttributeContainer(att, d.data[att]);
-          atts.add({d.id.toString() : atc.value});
+          atts.add({d.id.toString(): atc.value});
         }
       });
-      end = json.encode(atts);
+      String end = json.encode(atts);
+      var response = r.response;
+      response.write(end);
+      response.close();
       print(end);
+    } else {
+      String end = "Could not find collection $query";
+      print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
     }
+  } else if (path == "/query/newId") {
+    print("New ID query from ${r.connectionInfo.remoteAddress}");
+    String query = r.uri.queryParameters["q"];
+    int id = int.parse(r.uri.queryParameters["id"]);
+    print("Queried $query for new id $id");
+    if (dbs.any((Collection value) => value.name == query)) {
+      Collection c = dbs.singleWhere((col) => col.name == query);
+      DataObject data =
+          c.dataList.singleWhere((d) => d.id == id, orElse: () => null);
+      if (data == null) {
+        String end = json.encode(true);
+        var response = r.response;
+        response.write(end);
+        response.close();
+        print(end);
+      } else {
+        String end = "Object $id is not available in $query";
+        var response = r.response;
+        response.write(end);
+        response.close();
+        print(end);
+      }
+    } else {
+      String end = "Could not find collection $query";
+      print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
+    }
+  } else {
+    print("Error: Invalid GET request from ${r.connectionInfo.remoteAddress}");
+    print("\'" + r.uri.path + "\'");
+    var response = r.response;
+    response.write("Error: Invalid request");
+    response.close();
   }
-
-  var response = r.response;
-  response.write(end);
-  response.close();
 }
 
 void handlePost(HttpRequest r) {
-  String end = "";
   String path = r.uri.path;
 
   if (path == "/add") {
@@ -162,30 +232,52 @@ void handlePost(HttpRequest r) {
       Collection c = dbs.singleWhere((col) => col.name == add);
       c.dataList.add(new DataObject.emptyMap(id));
       c.updateFile();
-      print("Successfully added object $id to $add");
+      String end = "Successfully added object $id to $add";
+      print(end);
       var response = r.response;
-      response.write("Successfully added object $id to $add");
+      response.write(end);
+      response.close();
+    } else {
+      String end = "Could not find collection $add";
+      print(end);
+      var response = r.response;
+      response.write(end);
       response.close();
     }
-  } else if (path == "/add/obj") {
+  } else if (path == "/add/object") {
     print("Object add request from ${r.connectionInfo.remoteAddress}");
     String add = r.uri.queryParameters["q"];
     Future<String> content = utf8.decodeStream(r);
     if (dbs.any((Collection value) => value.name == add)) {
       Collection c = dbs.singleWhere((col) => col.name == add);
+      DataObject d;
       content.then((result) {
-        DataObject d = new DataObject.fromJsonString(result);
-        print("Requested to add object ${d.id} to $add");
-        c.dataList.add(d);
-        c.updateFile();
-        print("\nSuccessfully added this object to $add");
-        var response = r.response;
-        response.write("\nSuccessfully added this object to $add");
-        response.close();
+        try {
+          d = new DataObject.fromJsonString(result);
+          print("Requested to add object ${d.id} to $add");
+          c.dataList.add(d);
+          c.updateFile();
+          String end = "\nSuccessfully added this object to $add";
+          print(end);
+          var response = r.response;
+          response.write(end);
+          response.close();
+        } catch (e) {
+          String end = "Could not parse DataObject from request body\n" + e;
+          print(end);
+          var response = r.response;
+          response.write(end);
+          response.close();
+        }
       });
+    } else {
+      String end = "Could not find collection $add";
+      print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
     }
   } else if (path == "/add/attribute") {
-    String newEnd = "";
     print("Attribute add request from ${r.connectionInfo.remoteAddress}");
     String add = r.uri.queryParameters["q"];
     int id = int.parse(r.uri.queryParameters["id"]);
@@ -196,24 +288,41 @@ void handlePost(HttpRequest r) {
       content.then((result) {
         AttributeContainer attribute =
             new AttributeContainer(att, json.decode(result)[att]);
-        DataObject data = c.dataList.singleWhere((d) => d.id == id);
+        DataObject data =
+            c.dataList.singleWhere((d) => d.id == id, orElse: () => null);
         print("Requested to add attribute ${attribute.key} to $id in $add");
-        if (!data.data.containsKey(attribute.key)) {
-          data.data[att] = attribute.value;
-          c.updateFile();
-          print("Successfully added $att to $id");
-          var response = r.response;
-          response.write("Successfully added $att to $id");
-          response.close();
+        if (data != null) {
+          if (!data.data.containsKey(attribute.key)) {
+            data.data[att] = attribute.value;
+            c.updateFile();
+            String end = "Successfully added $att to $id";
+            print(end);
+            var response = r.response;
+            response.write(end);
+            response.close();
+          } else {
+            String end = "The attribute $att already exists in object $id";
+            print(end);
+            var response = r.response;
+            response.write(end);
+            response.close();
+          }
         } else {
-          print("The attribute $att already exists in object $id");
+          String end = "Could not find object $id in $add";
+          print(end);
           var response = r.response;
-          response.write("The attribute $att already exists in object $id");
+          response.write(end);
           response.close();
         }
       });
+    } else {
+      String end = "Could not find collection $add";
+      print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
     }
-  } else if (path == "/mod") {
+  } else if (path == "/mod/object") {
     print("Object mod request from ${r.connectionInfo.remoteAddress}");
     String mod = r.uri.queryParameters["q"];
     int id = int.parse(r.uri.queryParameters["id"]);
@@ -221,16 +330,31 @@ void handlePost(HttpRequest r) {
     print("Requested to change $id to $newId in $mod");
     if (dbs.any((Collection value) => value.name == mod)) {
       Collection c = dbs.singleWhere((col) => col.name == mod);
-      DataObject data = c.dataList.singleWhere((d) => d.id == id);
-      data.id = newId;
-      c.updateFile();
-      print("Successfully modified $id to $newId");
+      DataObject data =
+          c.dataList.singleWhere((d) => d.id == id, orElse: () => null);
+      if (data != null) {
+        data.id = newId;
+        c.updateFile();
+        String end = "Successfully modified $id to $newId";
+        print(end);
+        var response = r.response;
+        response.write(end);
+        response.close();
+      } else {
+        String end = "Could not find Object $id in $mod";
+        print(end);
+        var response = r.response;
+        response.write(end);
+        response.close();
+      }
+    } else {
+      String end = "Could not find collection $mod";
+      print(end);
       var response = r.response;
-      response.write("Successfully modified $id to $newId");
+      response.write(end);
       response.close();
     }
   } else if (path == "/mod/attribute") {
-    String newEnd = "";
     print("Attribute mod request from ${r.connectionInfo.remoteAddress}");
     String mod = r.uri.queryParameters["q"];
     int id = int.parse(r.uri.queryParameters["id"]);
@@ -242,26 +366,42 @@ void handlePost(HttpRequest r) {
         AttributeContainer attribute =
             new AttributeContainer(att, json.decode(result)[att]);
         print("Requested to modify attribute ${attribute.key} of $id in $mod");
-        DataObject data = c.dataList.singleWhere((d) => d.id == id);
-
-        if (data.data.containsKey(att)) {
-          data.data[att] = attribute.value;
-          c.updateFile();
-          print("Successfully modified $att of $id in $mod");
-          var response = r.response;
-          response.write("Successfully modified $att of $id in $mod");
-          response.close();
+        DataObject data =
+            c.dataList.singleWhere((d) => d.id == id, orElse: () => null);
+        if (data != null) {
+          if (data.data.containsKey(att)) {
+            data.data[att] = attribute.value;
+            c.updateFile();
+            String end = "Successfully modified $att of $id in $mod";
+            print(end);
+            var response = r.response;
+            response.write(end);
+            response.close();
+          } else {
+            String end = "Could not find attribute $att in object $id";
+            print(end);
+            var response = r.response;
+            response.write(end);
+            response.close();
+          }
         } else {
-          print("The attribute $att does not exist in object $id");
+          String end = "Could not find object $id";
+          print(end);
           var response = r.response;
-          response.write("The attribute $att does not exist in object $id");
+          response.write(end);
           response.close();
         }
       });
+    } else {
+      String end = "Could not find collection $mod";
+      print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
     }
-    end = newEnd;
   } else {
-    print("Error: Invalid request from ${r.connectionInfo.remoteAddress}");
+    print("Error: Invalid POST request from ${r.connectionInfo.remoteAddress}");
+    print("\'" + r.uri.path + "\'");
     var response = r.response;
     response.write("Error: Invalid request");
     response.close();
@@ -271,8 +411,86 @@ void handlePost(HttpRequest r) {
 void handlePut(HttpRequest r) {}
 void handleHead(HttpRequest r) {}
 void handleDelete(HttpRequest r) {
-  String m = r.method;
-  r.response.write("this is a $m");
+  String path = r.uri.path;
+  if (path == "/delete/object") {
+    print("Object delete request from ${r.connectionInfo.remoteAddress}");
+    String del = r.uri.queryParameters["q"];
+    int id = int.parse(r.uri.queryParameters["id"]);
+    if (dbs.any((Collection value) => value.name == del)) {
+      Collection c = dbs.singleWhere((col) => col.name == del);
+      print("Requested to delete object $id in $del");
+      DataObject data =
+          c.dataList.singleWhere((d) => d.id == id, orElse: () => null);
+      if (data != null) {
+        c.dataList.remove(data);
+        c.updateFile();
+        String end = "Successfully deleted object $id from $del";
+        print(end);
+        var response = r.response;
+        response.write(end);
+        response.close();
+      } else {
+        String end = "Could not find object $id in $del";
+        print(end);
+        var response = r.response;
+        response.write(end);
+        response.close();
+      }
+    } else {
+      String end = "Could not find collection $del";
+      print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
+    }
+  } else if (path == "/delete/attribute") {
+    print("Attribute delete request from ${r.connectionInfo.remoteAddress}");
+    String del = r.uri.queryParameters["q"];
+    int id = int.parse(r.uri.queryParameters["id"]);
+    String att = r.uri.queryParameters["a"];
+    if (dbs.any((Collection value) => value.name == del)) {
+      Collection c = dbs.singleWhere((col) => col.name == del);
+      print("Requested to delete object $id in $del");
+      DataObject data =
+          c.dataList.singleWhere((d) => d.id == id, orElse: () => null);
+      if (data != null) {
+        if (data.data.containsKey(att)) {
+          data.data.remove(att);
+          c.updateFile();
+          String end = "Successfully deleted attribute $att from $id";
+          print(end);
+          var response = r.response;
+          response.write(end);
+          response.close();
+        } else {
+          String end = "Could not find attribute $att in $id";
+          print(end);
+          var response = r.response;
+          response.write(end);
+          response.close();
+        }
+      } else {
+        String end = "Could not find object $id in $del";
+        print(end);
+        var response = r.response;
+        response.write(end);
+        response.close();
+      }
+    } else {
+      String end = "Could not find collection $del";
+      print(end);
+      var response = r.response;
+      response.write(end);
+      response.close();
+    }
+  } else {
+    print(
+        "Error: Invalid DELETE request from ${r.connectionInfo.remoteAddress}");
+    print("\'" + r.uri.path + "\'");
+    var response = r.response;
+    response.write("Error: Invalid request");
+    response.close();
+  }
 }
 
 void handlePatch(HttpRequest r) {}
