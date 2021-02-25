cf2962492915521072ec593cae667faa17ef36fc
Matt C <mcompton2002@gmail.com>
2021-02-25 00:25:59 -0500

    Update my testing script

--- testing_copy.sh
+++ testing_copy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+rm -rfv TESTING
+
 cp -rv Server TESTING
 
 pushd TESTING
 rm Databases/db.json
+echo "[]" > Databases/SparkPlug.json
+sed -i 's/admin/SparkPlug/g' data.jserv
 
 dart main.dart
 
